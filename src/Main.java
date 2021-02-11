1b9792e19742b3a8b8fd3ffdd2caf7f23e8aea5d
Richard Kolm <richard.kolm@gmx.at>
2021-02-11 11:19:28 +0100

    add exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        String vorname = "Richard";
+        String surname = "Kolm";
+        int age = 49;
+        String gender = "male";
+        Date dayOfBirth = new Date(63154800000L);
+        int gradePointAverage = 3;
+        boolean married = false;
+        System.out.println("Hello! i am " + vorname + " " + surname);
+        System.out.println("My age is " + age);
+        System.out.println("My gender is " + gender);
+        System.out.println("My day of birthday is " + dayOfBirth);
+        System.out.println("My grade point average is " + gradePointAverage);
+        System.out.println("My status of marital is " + married);
+
     }
 }
